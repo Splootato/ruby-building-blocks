1a8f87599bd8d16df857c85504961c39d227b0b1
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-04-07 13:04:21 -0400

    fixed indentation

--- stock.rb
+++ stock.rb
@@ -1,8 +1,8 @@
 def stock_picker(prices)
     differences = prices.map.with_index {|price, index|
-    if index != (prices.length - 1)
-        price = prices[index+1..-1].max - price
-    end }
+        if index != (prices.length - 1)
+            price = prices[index+1..-1].max - price
+        end }
     buy_day = differences.index(differences[0..-2].max)
     [buy_day, prices.index(prices[buy_day..-1].max)]
 end
