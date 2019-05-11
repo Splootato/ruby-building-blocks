2265c538f2c34e92bc448671d159a672deae4547
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-05-11 14:10:50 -0400

    completed bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -13,3 +13,18 @@ def bubble_sort(array)
     end
     array
 end
+def bubble_sort_by(array)
+    sorted,sort_iteration = false,1
+    until sort_iteration == array.length || sorted do
+        sorted,element = true,0
+        until element == array.length - sort_iteration do
+            if yield(array[element],array[element+1]) > 0
+                array[element],array[element+1] = array[element+1],array[element]
+                sorted = false
+            end
+            element += 1
+        end
+        sort_iteration += 1
+    end
+    array
+end
